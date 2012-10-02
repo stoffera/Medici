4ea55b7be406f5c90b87aa8ec1b3f93520ee86b7
Kristoffer Andersen <stoffera@gmail.com>
2012-10-03 00:08:11 +0200

    copyright header and file description

--- proxy.js
+++ proxy.js
@@ -1,10 +1,25 @@
+/**
+* Medici
+* Copyright (c) 2012 Kristoffer Andersen
+* All right reserved
+*
+* This file contains a server redirect proxy.
+* It can route incomming HTTP Requests on one listening port
+* to several other ports. This allows you to run a node.js server on port 8080
+* and a lighttpd server on 88. This proxy will listen on port 80, and route
+* requests to either based on the HOST request header. (like virtual hosts).
+* Should you choose to use this application, then change this host name in
+* line 22.
+*/
+
+
 var n = {
 	http : require('http')
 };
 
 var server = n.http.createServer(function(req, res){
 	var pReq = null;
-	if (req.headers.host == 'video.trik.dk') {
+	if (req.headers.host == 'subdomain.yourhost.com') {
 		pReq = n.http.request({
 			host: 'localhost',
 			port: 8080,
